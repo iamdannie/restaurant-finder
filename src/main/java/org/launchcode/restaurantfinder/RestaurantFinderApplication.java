7f459a52dffbda8470744a83cd0e434bcc24f78e
iamdannie <iamtaiyco@gmail.com>
2021-04-27 21:00:49 -0500

    Initial commit

---
+++ src/main/java/org/launchcode/restaurantfinder/RestaurantFinderApplication.java
@@ -0,0 +1,13 @@
+package org.launchcode.restaurantfinder;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestaurantFinderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestaurantFinderApplication.class, args);
+	}
+
+}
